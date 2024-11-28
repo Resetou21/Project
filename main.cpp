8befbd6636add362cceb5fea68a4020fe71a2731
resetou <153208341+resetou@users.noreply.github.com>
2024-11-28 11:07:29 +0100

    Implémentation de  Cell.h dans mon main

--- main.cpp
+++ main.cpp
@@ -4,53 +4,62 @@
 #include <ctime>
 #include <cstdlib>
 #include <fstream>
-#include <Cell.h>
+#include "Cell.h"
 using namespace std;
 
-
-// Configuration globale
 const int cellSize = 10;
 const int gridWidth = 80;
 const int gridHeight = 80;
 
 // Grille de cellules
-vector<vector<int>> grid(gridWidth, vector<int>(gridHeight));
+vector<vector<Cell*>> grid(gridWidth, vector<Cell*>(gridHeight));
 
-// Fonction d'initialisation de la grille
 void initializeGrid() {
     srand(time(0));
     for (int x = 0; x < gridWidth; ++x) {
         for (int y = 0; y < gridHeight; ++y) {
-            grid[x][y] = rand() % 2;  // Initialise aléatoirement les cellules comme vivantes ou mortes
+            // Initialiser aléatoirement les cellules
+            int randomCellType = rand() % 3;
+            if (randomCellType == 0) {
+                grid[x][y] = new LifeCell();  // Cellule vivante
+            } else if (randomCellType == 1) {
+                grid[x][y] = new CelluleMorte();  // Cellule morte
+            } else {
+                grid[x][y] = new CelluleObstacle();  // Cellule obstacle
+            }
         }
     }
 }
 
-// Fonction de rendu de la grille
 void renderGrid(sf::RenderWindow &window) {
     window.clear();
     sf::RectangleShape cell(sf::Vector2f(cellSize - 1.0f, cellSize - 1.0f));
-    cell.setFillColor(sf::Color::White);
 
     for (int x = 0; x < gridWidth; ++x) {
         for (int y = 0; y < gridHeight; ++y) {
-            if (grid[x][y] == 1) {
-                cell.setPosition(x * cellSize, y * cellSize);
-                window.draw(cell);
+            // Vérifier si la cellule est vivante
+            if (grid[x][y]->estVivante()) {
+                cell.setFillColor(sf::Color::White);  // Cellule vivante en blanc
+            } else {
+                cell.setFillColor(sf::Color::Black);  // Cellule morte en noir
             }
+
+            cell.setPosition(x * cellSize, y * cellSize);
+            window.draw(cell);
         }
     }
+
     window.display();
 }
 
 int main() {
-    // Fenêtre SFML
+
     sf::RenderWindow window(sf::VideoMode(gridWidth * cellSize, gridHeight * cellSize), "Game of Life");
 
-    // Initialiser la grille
+
     initializeGrid();
 
-    // Boucle principale
+
     while (window.isOpen()) {
         sf::Event event;
         while (window.pollEvent(event)) {
@@ -58,11 +67,18 @@ int main() {
                 window.close();
         }
 
-        // Afficher la grille
+
         renderGrid(window);
 
-        // Pause entre les frames
+ 
         sf::sleep(sf::milliseconds(100));
+    }
+
+
+    for (int x = 0; x < gridWidth; ++x) {
+        for (int y = 0; y < gridHeight; ++y) {
+            delete grid[x][y];  
+        }
     }
 
     return 0;
