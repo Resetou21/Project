73331cb88ae02b714ec6fd030e87465e8cb79a41
resetou <153208341+resetou@users.noreply.github.com>
2024-11-28 10:15:37 +0100

    Co-authored-by: Copcorico <Copcorico@users.noreply.github.com>

--- main.cpp
+++ main.cpp
@@ -1,30 +1,41 @@
-#include <SFML/Graphics.hpp>
+#include <iostream>
 #include <vector>
+#include <SFML/Graphics.hpp>
 #include <ctime>
 #include <cstdlib>
+#include <fstream>
 
+using namespace std;
+
+class Cell {
+    virtual bool cellState() 
+}
+// Configuration globale
 const int cellSize = 10;
 const int gridWidth = 80;
 const int gridHeight = 80;
 
+// Grille de cellules
 std::vector<std::vector<int>> grid(gridWidth, std::vector<int>(gridHeight));
 
+// Fonction d'initialisation de la grille
 void initializeGrid() {
     std::srand(std::time(0));
     for (int x = 0; x < gridWidth; ++x) {
         for (int y = 0; y < gridHeight; ++y) {
-            grid[x][y] = std::rand() % 2;  // Randomly initialize cells as alive or dead
+            grid[x][y] = std::rand() % 2;  // Initialise aléatoirement les cellules comme vivantes ou mortes
         }
     }
 }
 
+// Fonction de rendu de la grille
 void renderGrid(sf::RenderWindow &window) {
-    int x, y;
-    
     window.clear();
     sf::RectangleShape cell(sf::Vector2f(cellSize - 1.0f, cellSize - 1.0f));
-    for (x = 0; x < gridWidth; ++x) {
-        for (y = 0; y < gridHeight; ++y) {
+    cell.setFillColor(sf::Color::White);
+
+    for (int x = 0; x < gridWidth; ++x) {
+        for (int y = 0; y < gridHeight; ++y) {
             if (grid[x][y] == 1) {
                 cell.setPosition(x * cellSize, y * cellSize);
                 window.draw(cell);
@@ -35,10 +46,13 @@ void renderGrid(sf::RenderWindow &window) {
 }
 
 int main() {
+    // Fenêtre SFML
     sf::RenderWindow window(sf::VideoMode(gridWidth * cellSize, gridHeight * cellSize), "Game of Life");
-    
+
+    // Initialiser la grille
     initializeGrid();
 
+    // Boucle principale
     while (window.isOpen()) {
         sf::Event event;
         while (window.pollEvent(event)) {
@@ -46,8 +60,10 @@ int main() {
                 window.close();
         }
 
+        // Afficher la grille
         renderGrid(window);
 
+        // Pause entre les frames
         sf::sleep(sf::milliseconds(100));
     }
 
